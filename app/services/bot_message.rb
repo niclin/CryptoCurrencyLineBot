cfefb034bf56e20f39aa472507bb23d738bf1862
niclin <bboyceo@hotmail.com>
2018-03-21 22:50:13 +0800

    Add bot message center

---
+++ app/services/bot_message.rb
@@ -0,0 +1,10 @@
+module BotMessage
+  module_function
+
+  def help
+    "[ 指令說明 ]
+     [ BTC查詢 ] bot btc
+     [ 支援幣種 ] bot eth/eos/qtum
+     [ 作者 ] https://github.com/niclin"
+  end
+end
--- app/services/line_bot_response_service.rb
+++ app/services/line_bot_response_service.rb
@@ -10,18 +10,31 @@ class LineBotResponseService
     # 拿到發話方的文字
     message_type = @params['events'][0]["message"]["type"]
     message_text = @params['events'][0]["message"]["text"]
+    response_message = ""
 
-    return "" if message_type != "text" || !message_text.start_with?("bot")
+    if message_type == "text" || message_text.start_with?("bot")
+      message_text.slice!("bot ")
+      key_word = message_text.strip
 
-    response_message
+      response_message =
+      case key_word
+      when "help" then BotMessage.help
+      else
+        "指令錯誤，輸入 bot help 瞭解完整指令。"
+      end
+    end
+
+    return nil if response_message.blank?
+
+    response_message(response_message)
   end
 
   private
 
-  def response_message
+  def response_message(message)
     {
       type: "text",
-      text: "指令輸入對了"
+      text: message
     }
   end
 end
