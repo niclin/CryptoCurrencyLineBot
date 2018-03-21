2eb1c87eac2e6ac597a58d2dc294793949b1f594
niclin <bboyceo@hotmail.com>
2018-03-21 22:55:10 +0800

    Minor adjustment

--- app/services/bot_message.rb
+++ app/services/bot_message.rb
@@ -3,8 +3,8 @@ module BotMessage
 
   def help
     "[ 指令說明 ]
-     [ BTC查詢 ] bot btc
-     [ 支援幣種 ] bot eth/eos/qtum
-     [ 作者 ] https://github.com/niclin"
+[ BTC查詢 ] bot btc
+[ 支援幣種 ] bot eth/eos/qtum
+[ 作者 ] https://github.com/niclin"
   end
 end
