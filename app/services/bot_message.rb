4b58e22aef46b88e3a2e8d6a28319fc1b7f7550a
niclin <bboyceo@hotmail.com>
2018-03-22 14:02:27 +0800

    Add version

--- app/services/bot_message.rb
+++ app/services/bot_message.rb
@@ -2,7 +2,7 @@ module BotMessage
   module_function
 
   def help
-    "[ 指令說明 ]\n[ BTC查詢 ] bot btc\n[ 支援幣種 ] bot eth/eos/qtum\n[ 作者 ] https://github.com/niclin"
+    "[ 指令說明 ]\n[ BTC查詢 ] bot btc\n[ 支援幣種 ] bot eth/eos/qtum\n[ 作者 ] https://github.com/niclin\n[ 版本 ] v 1.0.0.beta"
   end
 
   def author
