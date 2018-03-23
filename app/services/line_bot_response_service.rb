e73ab5827422838fbcc1af586d1d669698307652
niclin <bboyceo@hotmail.com>
2018-03-23 13:18:30 +0800

    Fix bug

--- app/services/line_bot_response_service.rb
+++ app/services/line_bot_response_service.rb
@@ -37,7 +37,7 @@ class LineBotResponseService
   end
 
   def response_by_key_word(key_word)
-    return BotMessage.currency_price_info(key_word) if Settings.crypto_currencies.include?("key_word")
+    return BotMessage.currency_price_info(key_word) if Settings.crypto_currencies.include?(key_word)
     return BotMessage.help if key_word == "help"
     return BotMessage.author if key_word == "nic"
     return "指令錯誤，輸入 bot help 瞭解完整指令。" 
