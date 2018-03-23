3361c1cf87e6cf8c2c752e1e42bdd88ae231ab00
niclin <bboyceo@hotmail.com>
2018-03-23 13:22:19 +0800

    Update help

--- app/services/bot_message.rb
+++ app/services/bot_message.rb
@@ -2,7 +2,18 @@ module BotMessage
   module_function
 
   def help
-    "[指令說明]\n[BTC查詢] bot btc\n[支援幣種] bot eth/eos/qtum\n[作者] https://github.com/niclin\n[版本] v 1.0.0.beta"
+    "[指令說明]\n[BTC查詢] bot btc\n#{support_currencies}\n[作者] https://github.com/niclin\n[版本] v 1.0.0.beta"
+  end
+
+  def support_currencies
+    title = "[支援幣種]"
+    content = "bot "
+
+    Settings.crypto_currencies.each do |currency|
+      content.concat("#{currency}/")
+    end
+
+    "#{title} #{content}"
   end
 
   def author
