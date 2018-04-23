6a0429797478702ac003832b0a17e0a0cddfea0b
niclin <bboyceo@hotmail.com>
2018-04-23 15:16:37 +0800

    Implement fiat currency conveter

--- app/services/fiat_currency_converter.rb
+++ app/services/fiat_currency_converter.rb
@@ -1,10 +1,30 @@
 class FiatCurrencyConverter
   class << self
     def exchange(amount:, from:, to:)
-      from_currency = Money::Currency.new(from)
-      rate = Money.new(1, from_currency).exchange_to(to).to_d * from_currency.subunit_to_unit
+      if from == "usdt"
+        from_currency = Money::Currency.new("usd")
+        rate = Money.new(usdt_to_usd_rate, from_currency).exchange_to(to).to_d * from_currency.subunit_to_unit
+      else
+        from_currency = Money::Currency.new(from)
+        rate = Money.new(1, from_currency).exchange_to(to).to_d * from_currency.subunit_to_unit
+      end
 
-      amount.to_d * rate
+      (amount.to_d * rate).round(2)
+    end
+
+    private
+
+    def usdt_to_usd_rate
+      Rails.cache.fetch("#{Time.zone.today}-usdt-rate", expires_in: 60.seconds) do
+        response = RestClient.get("https://api.coinmarketcap.com/v1/ticker/?limit=0")
+
+        raise Error, "APIError, response: #{response}" if response.code != 200
+        response_body = JSON.parse(response)
+
+        currency_data = response_body.find { |currency_data| currency_data["symbol"] == "USDT" }
+
+        currency_data["price_usd"].to_f
+      end
     end
   end
 end
