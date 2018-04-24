8ccebf5b010b5e5451692d25e1c70d0576145f2c
niclin <bboyceo@hotmail.com>
2018-04-24 12:39:15 +0800

    Refactor

--- app/services/fiat_currency_converter.rb
+++ app/services/fiat_currency_converter.rb
@@ -1,14 +1,10 @@
 class FiatCurrencyConverter
   class << self
     def exchange(amount:, from:, to:)
-      if from == "usdt"
-        to = "usd" if to == "usdt"
-        from_currency = Money::Currency.new("usd")
-        rate = Money.new(usdt_to_usd_rate, from_currency).exchange_to(to).to_d * from_currency.subunit_to_unit
-      else
-        from_currency = Money::Currency.new(from)
-        rate = Money.new(1, from_currency).exchange_to(to).to_d * from_currency.subunit_to_unit
-      end
+      from, to = "usd", "usd" if from == "usdt"
+
+      from_currency = Money::Currency.new(from)
+      rate = Money.new(basic_unit(from), from_currency).exchange_to(to).to_d * from_currency.subunit_to_unit
 
       (amount.to_d * rate).round(2)
     end
@@ -26,6 +22,10 @@ class FiatCurrencyConverter
 
         currency_data["price_usd"].to_f
       end
+    end
+
+    def basic_unit(from)
+      from == "usdt" ? usdt_to_usd_rate : 1
     end
   end
 end
