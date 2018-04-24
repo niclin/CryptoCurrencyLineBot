bb89f90c721c5cdc50aebebbdcfcab3d2d33939a
niclin <bboyceo@hotmail.com>
2018-04-24 12:43:58 +0800

    Revert

--- app/services/fiat_currency_converter.rb
+++ app/services/fiat_currency_converter.rb
@@ -1,11 +1,14 @@
 class FiatCurrencyConverter
   class << self
     def exchange(amount:, from:, to:)
-      basic_unit = basic_unit(from)
-      from, to = "usd", "usd" if from == "usdt"
-
-      from_currency = Money::Currency.new(from)
-      rate = Money.new(basic_unit, from_currency).exchange_to(to).to_d * from_currency.subunit_to_unit
+      if from == "usdt"
+        to = "usd" if to == "usdt"
+        from_currency = Money::Currency.new("usd")
+        rate = Money.new(usdt_to_usd_rate, from_currency).exchange_to(to).to_d * from_currency.subunit_to_unit
+      else
+        from_currency = Money::Currency.new(from)
+        rate = Money.new(1, from_currency).exchange_to(to).to_d * from_currency.subunit_to_unit
+      end
 
       (amount.to_d * rate).round(2)
     end
@@ -23,10 +26,6 @@ class FiatCurrencyConverter
 
         currency_data["price_usd"].to_f
       end
-    end
-
-    def basic_unit(from)
-      from == "usdt" ? usdt_to_usd_rate : 1
     end
   end
 end
